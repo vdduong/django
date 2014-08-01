e25859dfeed9f15b5768a605c7e0d1556d179917
vdduong <vdduong@free.fr>
2014-08-02 01:43:44 +0200

    Update standard_cgi.py

--- standard_cgi.py
+++ standard_cgi.py
@@ -1,1 +1,21 @@
 #!/usr/bin/python
+
+import MySQLdb
+
+print "Content-Type: text/html"
+print
+print "<html><head><title>Livres</title></head>"
+print "<body>"
+print "<h1>Livres</h1>"
+print "<ul>"
+
+connection = MySQLdb.connect(user='moi', passwd='laissezmoientrer',db='ma_base')
+cursor = connection.cursor()
+cursor.execute("SELECT nom FROM livres ORDER BY pub_name DESC LIMIT 10")
+for row in cursor.fetchall():
+  print "<li>%s</li>"% row[0]
+
+print "</ul>"
+print "</body></html>"
+
+connection.close()
