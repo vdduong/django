22fb2757e776ecb04479d4d6fb41d006694185b8
vdduong <vdduong@free.fr>
2014-08-02 19:50:20 +0200

    Create settings.py

---
+++ settings.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+#
+# va contenir les informations de connexion à la base de données
+# le salt qui permet de sécuriser les clefs de hashage
+# Django settings for tuto_sdz project
