e190c96502624585c5224e2cd977010abea07b2c
vdduong <vdduong@free.fr>
2014-08-02 19:48:41 +0200

    Update tuto_sdz.py

--- tuto_sdz.py
+++ tuto_sdz.py
@@ -1,1 +1,11 @@
 django-admin.py startproject tuto_sdz # démarrer un projet
+
+# __init__.py : un fichier qui définit le répertoire comme étant un module Python
+# manage.py : nous permet de faire des actions définies sur notre projet Django.
+# settings.py: fichier de configuration de notre projet, en grande partie, spécifique à la machine
+# urls.py: ce fichier est destiné à recevoir les modèles d'URL que nous définirons.
+
+python manage.py help 
+
+python manage.py runserver
+
